6858be120d7699466ff68bb738cd37f3075b18bd
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-01 16:35:40 -0700

    added empty data values to create items later

--- bamazon.sql
+++ bamazon.sql
@@ -5,8 +5,41 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT
     items_id VARCHAR(40) NOT NULL,
     product_name VARCHAR(75) NOT NULL,
     departemnt_name VARCHAR(75) NOT NULL,
     price INT(10,2) NOT NULL,
+    stock_quantity INT (4)
+    PRIMARY KEY()
 );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
+
+INSERT INTO products("items_id","product_name","deparment_name", , )
+VALUES (" ", " ", " ", , );
