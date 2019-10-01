6f2d104db5c46b559decbbdf3bc6e8df83e55ecb
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-01 16:28:58 -0700

    created new databasse tabel with calumns product_name, deparment_name, and price

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,12 @@
-Drop DATABASE IF EXISTS 
+Drop DATABASE IF EXISTS bamazon_DB;
+
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+    items_id VARCHAR(40) NOT NULL,
+    product_name VARCHAR(75) NOT NULL,
+    departemnt_name VARCHAR(75) NOT NULL,
+    price INT(10,2) NOT NULL,
+);
