60ebd99c274267a2e5778c48a383b513ef4fd745
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-01 18:34:38 -0700

    added data to fill up tables

--- bamazon.sql
+++ bamazon.sql
@@ -14,32 +14,32 @@ CREATE TABLE products (
     PRIMARY KEY()
 );
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Samsung 75in UHD TV ", "Electronics ", 1699.98 , 45 );
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Xbox ONE S ", "Electronics ", 250.00 , 100 );
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "HydroFlask-red 30oz ","dishware",45.00,20 );
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Standard Oak Desk ","furniture ", 349.99 ,15);
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "brown leather Chelsea boots ", "Clothing ",175.00,32);
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Queen size blanket-green", "Bedding",65.99, );
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Penzoil synthetic motor oil", "Auto ", 6.99 , 12); 
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Womans Red dress Size S", "Clothing ", 139.99 ,8);
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Blue sofa couch", "Furniture", 699.99, 6);
 
-INSERT INTO products("items_id","product_name","deparment_name", , )
-VALUES (" ", " ", " ", , );
+INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
+VALUES (" ", "Queen size bed frame -light grey", "Bedding", 250.00, 1);
