80f6538ea44362283fae546674f0ea4f93ef9850
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-02 16:56:00 -0700

    added the ability for cutomer to remove quantity from overall stock

--- bamazon.sql
+++ bamazon.sql
@@ -5,41 +5,41 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT
+    id INT NOT NULL AUTO_INCREMENT,
     items_id VARCHAR(40) NOT NULL,
     product_name VARCHAR(75) NOT NULL,
     departemnt_name VARCHAR(75) NOT NULL,
-    price INT(10,2) NOT NULL,
-    stock_quantity INT (4)
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT (4),
     PRIMARY KEY(id)
 );
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Samsung 75in UHD TV ", "Electronics ", 1699.98 , 45 );
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("ST", "Samsung 75in UHD TV ", "Electronics ", 1699.98 , 45 );
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Xbox ONE S ", "Electronics ", 250.00 , 100 );
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("X1S ", "Xbox ONE S ", "Electronics ", 250.00 , 100 );
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "HydroFlask-red 30oz ","dishware",45.00,20 );
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("HF30 ", "HydroFlask-red 30oz ","dishware",45.00,20 );
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Standard Oak Desk ","furniture ", 349.99 ,15);
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("SOD", "Standard Oak Desk ","furniture ", 349.99 ,15);
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "brown leather Chelsea boots ", "Clothing ",175.00,32);
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("Bl2", "brown leather Chelsea boots ", "Clothing ",175.00,32);
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Queen size blanket-green", "Bedding",65.99, );
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("QS3", "Queen size blanket-green", "Bedding",65.99,25);
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Penzoil synthetic motor oil", "Auto ", 6.99 , 12); 
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("P2E", "Penzoil synthetic motor oil", "Auto ", 6.99 , 12); 
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Womans Red dress Size S", "Clothing ", 139.99 ,8);
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("WR3", "Womans Red dress Size S", "Clothing ", 139.99 ,8);
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Blue sofa couch", "Furniture", 699.99, 6);
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("BSC6", "Blue sofa couch", "Furniture", 699.99, 6);
 
-INSERT INTO products(items_id,product_name,deparment_name, price,stock_quantity )
-VALUES (" ", "Queen size bed frame -light grey", "Bedding", 250.00, 1);
+INSERT INTO products(items_id,product_name,department_name, price,stock_quantity )
+VALUES ("QSBG", "Queen size bed frame -light grey", "Bedding", 250.00, 1);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,70 @@
 var inquirer = require("inquirer");
-var mysql   = require("sql");
+var mysql = require("mysql");
 
-inquirer
-  .prompt([
-    {
-        type:"input",
-        name:"product ID",
-        message:"What is the id for the product that you'd like to purchase?"
-    },
-    {
-        type:"input",
-        name:"product quantity",
-        message:"whats the quantity that you'd like to purchase?"
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'bamazon_db'
+});
+
+connection.connect();
+connection.query("SELECT * FROM products", function (err, result) {
+  console.table(result);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "productID",
+        message: "What is the id for the product that you'd like to purchase?"
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "whats the quantity that you'd like to purchase?"
+      }
+    ])
+    .then(function (answers) {
+      console.log(answers)
+      connection.query(`SELECT * FROM products WHERE items_id=?`, [answers.productID], function (error, results) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].stock_quantity);
+
+        if (parseInt(answers.quantity) <= parseInt(results[0].stock_quantity)) {
+          console.log("Enough in stock placing order")
+          //update database
+          var newQuantity = parseInt(results[0].stock_quantity) - parseInt(answers.quantity);
+          console.log(newQuantity);
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: newQuantity
+              },
+              {
+                items_id: answers.productID
+              }
+            ],
+            function (error) {
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              // start();
+              connection.query("SELECT * FROM products", function (err, result) {
+                console.table(result);
+              })
+            })
+        } else {
+          console.log("over quantity value")
+        }
+      });
     }
-  ])
-  .then( { });
+    );
+})
+
+  //TODO:Check if theres enough of the product in stock to place the order for the customer
+
+  //TODO:if there isn't enough of the product tell the customer 'insufficient quantity' and prevent the order from happening
+
+  //TODO:if there is enough product in stock place the order for the customer and update the quantity of the product 
+
+  //TODO:show the total cost of their order to the customer
