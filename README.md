c579c70932826460cd50a8aeee6ba0caee0b3b16
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-13 16:52:26 -0700

    updated read me info

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# bamazon
+# Bamazson
+
+### What is Bamazson
+-Bamazson is a CLI application that displays either movie or song info based on the title you give the program. 
+
+### So Why Bamazson?
+-The purpose of Bamazson's creation 
+
+### Installation Guide AND User Operation
+1. You'll first need to download the dependencies required for this program by opening the terminal add typing out this command: `npm install`
+
+<!-- add demo images here -->
+
+<!-- add demo images here -->
+
+### Technologies Used In Creating This Application
+    *Node.js
+    *MyqSQL
+    *Inquirer npm package
+    
+### Who Maintains And Contributes To The Project
+
+The maintenance and development of this project is held solely responsible by "Rogelio Zavala"
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,80 +4,81 @@ var cartTotal = 0;
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
+  //replace 'password' with your own password
   password: 'password',
   database: 'bamazon_db'
 });
 
 connection.connect();
 
-function start(){
+function start() {
 
-  
+
   connection.query("SELECT * FROM products", function (err, result) {
     console.table(result);
     inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "productID",
-        message: "What is the id for the product that you'd like to purchase?"
-      },
-      {
-        type: "input",
-        name: "quantity",
-        message: "whats the quantity that you'd like to purchase?"
-      }
-    ])
-    .then(function (answers) {
-      console.log(answers)
-      connection.query(`SELECT * FROM products WHERE id=?`, [answers.productID], function (error, results) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].stock_quantity);
-        
-        if (parseInt(answers.quantity) <= parseInt(results[0].stock_quantity)) {
-          console.log("Enough in stock placing order")
-        cartTotal +=  results[0].price;
-          //update database
-          var newQuantity = parseInt(results[0].stock_quantity) - parseInt(answers.quantity);
-          console.log(newQuantity);
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: newQuantity
-              },
-              {
-                id: answers.productID
-              }
-            ],
-            function (error) {
-              if (error) throw err;
-              console.log("Order made enjoy!!");
-               inquirer.prompt([
-                 {
-                   type:"list",
-                   name:"ContinueOrEnd",
-                   message:"Would you like to choose any more items?",
-                   choices:["yes","no"]
-                 }
-               ]).then(function(answers){
-                 if(answers["ContinueOrEnd"] === "yes"){
-                  start();
-                 }else{
-                   connection.end();
-                   console.log("Thanks for shopping, your total is:" + cartTotal);
-                 }
-               })
-            //  connection.query("SELECT * FROM products", function (err, result) {
-               // console.table(result);
-          //    })
-            })
-          } else {
-          console.log("over quantity value")
+      .prompt([
+        {
+          type: "input",
+          name: "productID",
+          message: "What is the id for the product that you'd like to purchase?"
+        },
+        {
+          type: "input",
+          name: "quantity",
+          message: "whats the quantity that you'd like to purchase?"
         }
-      });
-    }
-    );
+      ])
+      .then(function (answers) {
+        console.log(answers)
+        connection.query(`SELECT * FROM products WHERE id=?`, [answers.productID], function (error, results) {
+          if (error) throw error;
+          console.log('The solution is: ', results[0].stock_quantity);
+
+          if (parseInt(answers.quantity) <= parseInt(results[0].stock_quantity)) {
+            console.log("Enough in stock placing order")
+            cartTotal += results[0].price;
+            //update database
+            var newQuantity = parseInt(results[0].stock_quantity) - parseInt(answers.quantity);
+            console.log(newQuantity);
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: newQuantity
+                },
+                {
+                  id: answers.productID
+                }
+              ],
+              function (error) {
+                if (error) throw err;
+                console.log("Order made enjoy!!");
+                inquirer.prompt([
+                  {
+                    type: "list",
+                    name: "ContinueOrEnd",
+                    message: "Would you like to choose any more items?",
+                    choices: ["yes", "no"]
+                  }
+                ]).then(function (answers) {
+                  if (answers["ContinueOrEnd"] === "yes") {
+                    start();
+                  } else {
+                    connection.end();
+                    console.log("Thanks for shopping, your total is:" + cartTotal);
+                  }
+                })
+                //  connection.query("SELECT * FROM products", function (err, result) {
+                // console.table(result);
+                //    })
+              })
+          } else {
+            console.log("over quantity value")
+          }
+        });
+      }
+      );
   })
 }
 
